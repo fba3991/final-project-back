65ed9edebee461508bf02f5cf23cb36361867d3e
Fabioes <fabionike@icloud.com>
2024-02-22 02:15:53 +0100

    fix/ added cors link vercel

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://final-project-front-teal.vercel.app'],
+  origin: ['http://localhost:5173', 'https://final-project-front-gray.vercel.app'],
   credentials: true
 }));
 
