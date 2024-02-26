a904dbec43517d6b5a40262179c1fb386649276b
Fabioes <fabionike@icloud.com>
2024-02-26 17:11:40 +0100

    fix

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const app = express();
 
 // cors per permettere al front di funzionare
 app.use(cors({
-  origin:[ " http://localhost:5174/ ", 
-  "https://final-project-front-plum.vercel.app/ "],
+  origin:" http://localhost:5174 ", 
+  
 
   credentials: true // abilitazione delle credenziali
 }));
